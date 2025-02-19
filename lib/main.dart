219a22b117f19613ff8e925636a9353ffbf0a8e7
Arpon13 <arpon15-5655@diu.edu.bd>
2025-02-19 12:57:36 +0600

    Main Labtask of Today

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(lab5());
+  runApp(Lab5());
 }
 
-class lab5 extends StatelessWidget {
+class Lab5 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter UI",
+      title: "Flutter Watch UI",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter UI2"),
+          title: Text("Flutter Watch UI"),
           backgroundColor: Colors.blue,
           foregroundColor: Colors.white,
           centerTitle: true,
@@ -19,22 +19,70 @@ class lab5 extends StatelessWidget {
         body: Container(
           height: double.infinity,
           width: double.infinity,
-          color: Colors.greenAccent,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: List.generate
-              (100, 
-              (index)=>Icon(
-                  Icons.star,
-                  color: const Color.fromARGB(255, 17, 15, 1),
-                  size: index.toDouble(),
-                ),)),
-          ),
+          // padding: EdgeInsets.all(50),
+          margin: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10,
+                spreadRadius: 5,
+                offset: Offset(5, 5),
+              ),
+            ],
+          color: const Color.fromARGB(255, 161, 189, 241),),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 233, 225, 225),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text(
+                      "50% OFF",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                    size: 24,
+                  ),
+                  
+                ],
+              ),
+                  Image.asset(
+                    'assets/images/photo.png',
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                  ),
+                Text(
+                      "Nike Air Shoes",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                
+                  Text("#45,000",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text("#55,000",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, decoration: TextDecoration.lineThrough),
+                  ),
+                ],),
+              
+            ],
           ),
         ),
+      ),
     );
   }
 }
